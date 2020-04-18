correct factor 2 in gaussian beam definition

tools.py
@@ -103,7 +103,7 @@ def define_gaussian_beam(x, y, z, NA, n, grid_step, pol='X', dir=1):
 
     # amplitude of the E field
     w0 =  2 * np.pi * n 
-    zR = w0**2 / n
+    zR = w0**2 / (2*n)
 
     w = w0 * np.sqrt(1 + (z/zR)**2)
     R = dir * z * (1 + (zR/z)**2)